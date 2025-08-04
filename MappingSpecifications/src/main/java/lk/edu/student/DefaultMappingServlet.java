a384d839428db5ce6407d6dbde680e6232cfef91
Dev-Dolawatta <.dtdolawatta@gmail.com>
2025-08-04 10:33:32 +0530

    default mapping

---
+++ MappingSpecifications/src/main/java/lk/edu/student/DefaultMappingServlet.java
@@ -0,0 +1,18 @@
+package lk.edu.student;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class DefaultMappingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().print("Default Mapping");
+    }
+}
