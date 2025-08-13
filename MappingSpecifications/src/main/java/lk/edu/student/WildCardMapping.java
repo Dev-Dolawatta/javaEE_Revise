97ef06d8cbd143df42743375dee7f3d56f0a242c
Dev-Dolawatta <.dtdolawatta@gmail.com>
2025-08-13 13:49:13 +0530

    update wild card mapping

--- MappingSpecifications/src/main/java/lk/edu/student/WildCardMapping.java
+++ MappingSpecifications/src/main/java/lk/edu/student/WildCardMapping.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class WildCardMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //
         resp.getWriter().print("Wild Card Mapping");
     }
 }
